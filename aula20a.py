687216e1fe1617e432a4d7bb0cbd14a9cb064466
giancarlo.albano <giancarlo.albano@cresolconfederacao.com.br>
2019-02-24 09:30:34 -0300

    funções

--- aula20a.py
+++ aula20a.py
@@ -11,3 +11,24 @@ soma(a=2, b=3)
 soma(b=2, a=3)
 
 #é possivel passar parametros tradiconalmente e passar nomeando. Se nomear, nao precsa seguir a ordem de declaração
+
+#----------------
+#recebe quantidade aleatorias de parametros e transforma em tupla
+def contador(*num):
+    print(num)
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 6, 2)
+
+
+#-------------
+def soma2(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}')
+
+
+soma2(5, 2)
+soma2(2, 9, 4)
