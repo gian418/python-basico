93097bf64ab093b0369337d78e747363ceef8d3c
giancarlo.albano <giancarlo.albano@cresolconfederacao.com.br>
2019-02-24 09:53:32 -0300

    Docstrings e parametros opcionais

---
+++ aula21a.py
@@ -0,0 +1,11 @@
+def somar(a, b, c=0): #c é um parametro opcional
+    """
+    -> Isso aqui são as docstrings que explica sobre a funcao
+    :param a: bla bla bla
+    :param b: bla bla bla
+    :param c: bla bla bla
+    :return: bla bla bla
+    """
+    print("Qualquer logica")
+
+somar(1, 2)
